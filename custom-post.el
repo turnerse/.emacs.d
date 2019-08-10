63a3fdde71fa6cb18bda51989252246c71b5da65
Steve Turner <steve@turnerse.com>
2019-08-10 00:32:38 -0500

    Added slime/roswell

--- custom-post.el
+++ custom-post.el
@@ -4,3 +4,7 @@
 
 (setq mac-command-modifier 'meta) ;; default = 'meta
 (setq mac-option-modifier 'super) ;; default = none
+
+;; Slime
+(load (expand-file-name "~/.roswell/helper.el"))
+(setq inferior-lisp-program "ros -Q run")
