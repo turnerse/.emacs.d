8137ac07927ce02a57080f5066fa1f9d0e683a82
Steve Turner <steve@turnerse.com>
2020-12-18 01:11:07 -0600

    Update init-local.el

--- init-local.el
+++ init-local.el
@@ -1,17 +1,24 @@
 (require 'use-package)
 
 (use-package org-roam
-             :ensure t
-             :hook
-             (after-init . org-roam-mode)
-             :custom
-             (org-roam-directory "~/org-roam")
-             :bind (:map org-roam-mode-map
-                         (("C-c n l" . org-roam)
-                          ("C-c n f" . org-roam-find-file)
-                          ("C-c n g" . org-roam-graph))
-                         :map org-mode-map
-                         (("C-c n i" . org-roam-insert))
-                         (("C-c n I" . org-roam-insert-immediate))))
+  :ensure t
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory "~/org-roam")
+  :bind (:map org-roam-mode-map
+         (("C-c n l" . org-roam)
+          ("C-c n f" . org-roam-find-file)
+          ("C-c n g" . org-roam-graph))
+         :map org-mode-map
+         (("C-c n i" . org-roam-insert))
+         (("C-c n I" . org-roam-insert-immediate))))
+
+
+(require 'org-download)
+
+;; Drag-and-drop to `dired`
+(add-hook 'dired-mode-hook 'org-download-enable)
+
 
 (provide 'init-local)
